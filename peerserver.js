69af13d75d95b4856b25d3483004af32d6e14a1e
mightykim91 <mightykim91@naver.com>
2020-10-08 01:30:29 +0900

    Add node peer server

---
+++ peerserver.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const { PeerServer } = require("peer");
+
+const peerServer = PeerServer({
+  port: 9000,
+  ssl: {
+    key: fs.readFileSync("/etc/letsencrypt/live/j3a405.p.ssafy.io/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/j3a405.p.ssafy.io/cert.pem"),
+  },
+});
